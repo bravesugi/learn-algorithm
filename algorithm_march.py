924a04090fe1762a95df8c791c89b639859549bb
bravesugi <76844403+bravesugi@users.noreply.github.com>
2021-02-21 11:42:08 +0900

    Only for ahead

---
+++ algorithm_march.py
@@ -0,0 +1,34 @@
+def algorithm_march():
+    steps = 0
+
+    while (True):
+        steps += 1
+        print('一歩進んで前ならえ')
+        steps += 1
+        print('一歩進んで偉い人')
+        steps += 1
+        print('ひっくり返ってぺこりんこ')
+        steps += 1
+        print('横に歩いてキョロキョロ')
+        steps += 1
+        print('ちょっとここらで平泳ぎ')
+        steps += 1
+        print('ちょっとしゃがんでで栗拾い')
+        steps += 1
+        print('空気入れますシューシュー')
+        steps += 1
+        print('空気が入ってピューピュー')
+
+        if (steps >= 16):
+            break
+
+    steps += 1
+    print('そろそろ終わりかな')
+    steps += 1
+    print('そろそろ終わりかな')
+    steps += 1
+    print('そろそろ終わりかな')
+    print('終わり')
+
+if __name__ == '__main__':
+    algorithm_march()
